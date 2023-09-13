c739fe933cccb7b9addcf7ad1c5efb8bc272d5d5
Baldelli Andrea <andrea.baldelli2000@gmail.com>
2023-09-13 11:49:17 +0200

    Compute shader tests

--- Assets/Scripts/FluidSimulation.cs
+++ Assets/Scripts/FluidSimulation.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 [DisallowMultipleComponent]
@@ -8,21 +7,21 @@ public class FluidSimulation : MonoBehaviour
 {
     private class Fluid
     {
-        private int width;
-        private int height;
+        public int width;
+        public int height;
         private float verticesDistance;
         private float timeSlice;
         private float wavePropagationVelocity;
         private float viscosity;
 
-        public Vector3[] buffer1;
-        public Vector3[] buffer2;
+        public CustomVector3[] buffer1;
+        public CustomVector3[] buffer2;
         public int renderBuffer;
 
         private Vector3[] normal;
         private Vector3[] tangent;
 
-        private float k1, k2, k3;
+        public float k1, k2, k3;
 
         public Fluid(int width, int height, float verticesDistance, float timeSlice, float wavePropagationVelocity, float viscosity)
         {
@@ -34,8 +33,8 @@ public class FluidSimulation : MonoBehaviour
             this.viscosity = viscosity;
             int count = width * height;
 
-            buffer1 = new Vector3[count];
-            buffer2 = new Vector3[count];
+            buffer1 = new CustomVector3[count];
+            buffer2 = new CustomVector3[count];
             renderBuffer = 0;
 
             normal = new Vector3[count];
@@ -183,8 +182,8 @@ public class FluidSimulation : MonoBehaviour
 
         private void DisplaceVertices()
         {
-            Vector3[] currentBuffer = renderBuffer == 0 ? buffer1 : buffer2;
-            Vector3[] previousBuffer = renderBuffer == 1 ? buffer1 : buffer2;
+            CustomVector3[] currentBuffer = renderBuffer == 0 ? buffer1 : buffer2;
+            CustomVector3[] previousBuffer = renderBuffer == 1 ? buffer1 : buffer2;
 
             for (int j = 1; j < height - 1; j++)
                 for (int i = 1; i < width - 1; i++)
@@ -195,14 +194,14 @@ public class FluidSimulation : MonoBehaviour
                 }
         }
 
-        private void SwapBuffers()
+        public void SwapBuffers()
         {
             renderBuffer = 1 - renderBuffer;
         }
 
         private void CalculateNormalsAndTangents()
         {
-            Vector3[] nextBuffer = renderBuffer == 0 ? buffer1 : buffer2;
+            CustomVector3[] nextBuffer = renderBuffer == 0 ? buffer1 : buffer2;
 
             for (int j = 1; j < height - 1; j++)
                 for (int i = 1; i < width - 1; i++)
@@ -211,6 +210,17 @@ public class FluidSimulation : MonoBehaviour
                     normal[i + j * width].y = nextBuffer[i - width + j * width].z - nextBuffer[i + width + j * width].z;
                     tangent[i + j * width].z = nextBuffer[i + 1 + j * width].z - nextBuffer[i - 1 + j * width].z;
                 }
+        }
+    }
+
+    public struct CustomVector3
+    {
+        public float x, y, z;
+        public void Set(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
         }
     }
 
@@ -237,9 +247,14 @@ public class FluidSimulation : MonoBehaviour
 
     private Fluid fluid;
 
-    public Texture2D heightMap;
+    public RenderTexture heightMap;
+    //public Texture2D heightMap;
     private MeshRenderer planeRenderer;
     private Material planeMaterial;
+
+    public ComputeShader fluidSimulationCompute;
+    private ComputeBuffer fluidBuffer1;
+    private ComputeBuffer fluidBuffer2;
 
     void Awake()
     {
@@ -251,7 +266,7 @@ public class FluidSimulation : MonoBehaviour
         if (!drawGizmos)
             return;
 
-        Vector3[] currentBuffer = fluid.renderBuffer == 0 ? fluid.buffer1 : fluid.buffer2;
+        CustomVector3[] currentBuffer = fluid.renderBuffer == 0 ? fluid.buffer1 : fluid.buffer2;
         for (int i = 0; i < currentBuffer.Length; i++)
         {
             Gizmos.color = new Color((currentBuffer[i].x) / 10f, (currentBuffer[i].y) / 10f, 0);
@@ -292,63 +307,93 @@ public class FluidSimulation : MonoBehaviour
     void Start()
     {
         fluid = new Fluid(widthAndHeight, widthAndHeight, distance, timeSlice, wavePropagationVelocity, viscosity);
-        heightMap = new Texture2D(textureResolution, textureResolution, TextureFormat.R8, false);
-        heightMap.wrapMode = TextureWrapMode.Clamp;
-        heightMap.filterMode = FilterMode.Trilinear;
+
+        heightMap = new RenderTexture(widthAndHeight, widthAndHeight, 0, RenderTextureFormat.R8);
+        heightMap.enableRandomWrite = true;
+
+        //heightMap = new Texture2D(textureResolution, textureResolution, TextureFormat.R8, false);
+        //heightMap.wrapMode = TextureWrapMode.Clamp;
+        //heightMap.filterMode = FilterMode.Trilinear;
+
         planeMaterial = planeRenderer.material;
+        planeMaterial.mainTexture = heightMap;
     }
 
     void Update()
     {
-        if(isRaining)
+        if (isRaining)
         {
-            float randomX = Random.Range(0f, 1f);
-            float randomY = Random.Range(0f, 1f);
+            float randomX = UnityEngine.Random.Range(0f, 1f);
+            float randomY = UnityEngine.Random.Range(0f, 1f);
             fluid.Turbate(randomX, randomY, rainForce, 2);
         }
 
         if (Input.GetKeyDown(KeyCode.G))
         {
-            float randomX = Random.Range(0f, 1f);
-            float randomY = Random.Range(0f, 1f);
+            float randomX = UnityEngine.Random.Range(0f, 1f);
+            float randomY = UnityEngine.Random.Range(0f, 1f);
             fluid.Turbate(randomX, randomY, objectEnterForce, 2);
         }
     }
 
     void FixedUpdate()
     {
-        fluid.Evaluate();
+        fluidBuffer1 = new ComputeBuffer(widthAndHeight * widthAndHeight, 3 * sizeof(float));
+        fluidBuffer2 = new ComputeBuffer(widthAndHeight * widthAndHeight, 3 * sizeof(float));
 
-        Vector3[] currentBuffer = fluid.renderBuffer == 0 ? fluid.buffer1 : fluid.buffer2;
-        Color32[] colors = new Color32[textureResolution * textureResolution];
+        fluidBuffer1.SetData(fluid.buffer1);
+        fluidBuffer2.SetData(fluid.buffer2);
 
-        for (int i = 1; i < textureResolution - 1; i++)
-        {
-            float nearestX = Mathf.Lerp(0, (widthAndHeight - 1), Mathf.InverseLerp(0, textureResolution, i));
-            int nearestXUp = Mathf.CeilToInt(nearestX);
-            int nearestXDown = (int)nearestX;
-            float nearestXInterpolator = Mathf.Repeat(nearestX, 1f);
+        fluidSimulationCompute.SetBuffer(0, "CurrentBuffer", fluid.renderBuffer == 0 ? fluidBuffer1 : fluidBuffer2);
+        fluidSimulationCompute.SetBuffer(0, "PreviousBuffer", fluid.renderBuffer == 1 ? fluidBuffer1 : fluidBuffer2);
+        fluidSimulationCompute.SetTexture(0, "Result", heightMap);
+        fluidSimulationCompute.SetFloat("k1", fluid.k1);
+        fluidSimulationCompute.SetFloat("k2", fluid.k2);
+        fluidSimulationCompute.SetFloat("k3", fluid.k3);
+        fluidSimulationCompute.SetInt("width", fluid.width);
+        fluidSimulationCompute.SetInt("height", fluid.height);
+        fluidSimulationCompute.Dispatch(0, widthAndHeight / 8, widthAndHeight / 8, 1);
 
-            for (int j = 1; j < textureResolution - 1; j++)
-            {
-                float nearestY = Mathf.Lerp(0, (widthAndHeight - 1), Mathf.InverseLerp(0, textureResolution, j));
-                int nearestYUp = Mathf.CeilToInt(nearestY);
-                int nearestYDown = (int)nearestY;
-                float nearestYInterpolator = Mathf.Repeat(nearestY, 1f);
+        //fluid.Evaluate();
 
-                Vector3 currentVertex1 = currentBuffer[nearestXDown * widthAndHeight + nearestYDown];
-                Vector3 currentVertex2 = currentBuffer[nearestXDown * widthAndHeight + nearestYUp];
-                Vector3 currentVertex3 = currentBuffer[nearestXUp * widthAndHeight + nearestYUp];
-                Vector3 currentVertex4 = currentBuffer[nearestXUp * widthAndHeight + nearestYDown];
+        fluid.SwapBuffers();
 
-                float color = Mathf.InverseLerp(-0.2f, 0.2f, Mathf.Lerp(Mathf.Lerp(currentVertex1.z, currentVertex4.z, nearestXInterpolator), Mathf.Lerp(currentVertex2.z, currentVertex3.z, nearestXInterpolator), nearestYInterpolator));
-                colors[i * textureResolution + j] = new Color(color, color, color);
-            }
-        }
+        fluidBuffer1.GetData(fluid.buffer1);
+        fluidBuffer2.GetData(fluid.buffer2);
 
-        heightMap.SetPixels32(colors);
-        heightMap.Apply();
-        planeMaterial.mainTexture = heightMap;
+        fluidBuffer1.Release();
+        fluidBuffer2.Release();
+
+        //CustomVector3[] currentBuffer = fluid.renderBuffer == 0 ? fluid.buffer1 : fluid.buffer2;
+        //Color32[] colors = new Color32[textureResolution * textureResolution];
+
+        //for (int i = 1; i < textureResolution - 1; i++)
+        //{
+        //    float nearestX = Mathf.Lerp(0, (widthAndHeight - 1), Mathf.InverseLerp(0, textureResolution, i));
+        //    int nearestXUp = Mathf.CeilToInt(nearestX);
+        //    int nearestXDown = (int)nearestX;
+        //    float nearestXInterpolator = Mathf.Repeat(nearestX, 1f);
+
+        //    for (int j = 1; j < textureResolution - 1; j++)
+        //    {
+        //        float nearestY = Mathf.Lerp(0, (widthAndHeight - 1), Mathf.InverseLerp(0, textureResolution, j));
+        //        int nearestYUp = Mathf.CeilToInt(nearestY);
+        //        int nearestYDown = (int)nearestY;
+        //        float nearestYInterpolator = Mathf.Repeat(nearestY, 1f);
+
+        //        CustomVector3 currentVertex1 = currentBuffer[nearestXDown * widthAndHeight + nearestYDown];
+        //        CustomVector3 currentVertex2 = currentBuffer[nearestXDown * widthAndHeight + nearestYUp];
+        //        CustomVector3 currentVertex3 = currentBuffer[nearestXUp * widthAndHeight + nearestYUp];
+        //        CustomVector3 currentVertex4 = currentBuffer[nearestXUp * widthAndHeight + nearestYDown];
+
+        //        float color = Mathf.InverseLerp(-0.2f, 0.2f, Mathf.Lerp(Mathf.Lerp(currentVertex1.z, currentVertex4.z, nearestXInterpolator), Mathf.Lerp(currentVertex2.z, currentVertex3.z, nearestXInterpolator), nearestYInterpolator));
+        //        colors[i * textureResolution + j] = new Color(color, color, color);
+        //    }
+        //}
+        //heightMap.SetPixels32(colors);
+        //heightMap.Apply();
+        //planeMaterial.mainTexture = heightMap;
+
     }
 
 
