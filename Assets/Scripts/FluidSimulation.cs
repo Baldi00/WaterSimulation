bef8e5215ba2e6a429c93b15068fccd94c1b91fa
Baldelli Andrea <andrea.baldelli2000@gmail.com>
2023-10-09 12:36:59 +0200

    Improved water simulation with CustomRenderTexture

--- Assets/Scripts/FluidSimulation.cs
+++ Assets/Scripts/FluidSimulation.cs
@@ -1,4 +1,6 @@
+using Unity.Burst.CompilerServices;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 [DisallowMultipleComponent]
 [RequireComponent(typeof(MeshRenderer))]
@@ -38,9 +40,12 @@ public class FluidSimulation : MonoBehaviour
     private Material planeMaterial;
     public CustomRenderTexture waterSimulationRT;
 
+    private float verticesDistance;
+
     void Awake()
     {
         planeRenderer = GetComponent<MeshRenderer>();
+        planeMaterial = planeRenderer.material;
 
         ComputeCoefficients(out float k1, out float k2, out float k3);
 
@@ -48,17 +53,23 @@ public class FluidSimulation : MonoBehaviour
         waterSimulationRTMaterial.SetFloat("k2", k2);
         waterSimulationRTMaterial.SetFloat("k3", k3);
 
-        waterSimulationRT = new CustomRenderTexture(textureResolution, textureResolution, RenderTextureFormat.R8);
+        waterSimulationRT = new CustomRenderTexture(textureResolution, textureResolution, UnityEngine.Experimental.Rendering.GraphicsFormat.R32G32_SFloat);
         waterSimulationRT.material = waterSimulationRTMaterial;
         waterSimulationRT.updateMode = CustomRenderTextureUpdateMode.OnDemand;
         waterSimulationRT.initializationColor = Color.black;
+        waterSimulationRT.doubleBuffered = true;
+        waterSimulationRT.depth = 0;
+        waterSimulationRT.useMipMap = true;
+        waterSimulationRT.wrapMode = TextureWrapMode.Mirror;
 
-        waterSimulationRT.Update();
+        planeMaterial.mainTexture = waterSimulationRT;
+
+        //waterSimulationRT.Update();
     }
 
     private void ComputeCoefficients(out float k1, out float k2, out float k3)
     {
-        float verticesDistance = planeDefaultLength * transform.localScale.x / (textureResolution * textureResolution);
+        verticesDistance = planeDefaultLength * transform.localScale.x / (textureResolution * textureResolution);
         float f1 = wavePropagationVelocity * wavePropagationVelocity * timeSlice * timeSlice / (verticesDistance * verticesDistance);
         float f2 = 1 / (viscosity * timeSlice + 2);
         k1 = (4 - 8 * f1) * f2;
@@ -68,38 +79,93 @@ public class FluidSimulation : MonoBehaviour
 
     void Start()
     {
-        planeMaterial = planeRenderer.material;
+        
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            var clickZone = new CustomRenderTextureUpdateZone();
+            clickZone.needSwap = true;
+            clickZone.passIndex = 2;
+            clickZone.rotation = 0f;
+            clickZone.updateZoneCenter = new Vector2(0.5f, 0.5f);
+            clickZone.updateZoneSize = new Vector2(0.1f, 0.1f);
 
+            waterSimulationRT.SetUpdateZones(new CustomRenderTextureUpdateZone[] { clickZone });
+            waterSimulationRT.Update();
+        }
     }
 
     void FixedUpdate()
     {
-
+        var defaultZone = new CustomRenderTextureUpdateZone();
+        defaultZone.needSwap = true;
+        defaultZone.passIndex = 0;
+        defaultZone.rotation = 0f;
+        defaultZone.updateZoneCenter = new Vector2(0.5f, 0.5f);
+        defaultZone.updateZoneSize = new Vector2(1f, 1f);
+        waterSimulationRT.ClearUpdateZones();
+        waterSimulationRT.SetUpdateZones(new CustomRenderTextureUpdateZone[] { defaultZone });
+        waterSimulationRT.Update(2);
     }
 
     void OnTriggerEnter(Collider other)
     {
-        //Vector2 fluidCoord = WorldToFluidSpace(other.transform.position.x, other.transform.position.z);
-        //fluid.Turbate(fluidCoord.x, fluidCoord.y, objectEnterForce, 2);
+        var defaultZone = new CustomRenderTextureUpdateZone();
+        defaultZone.needSwap = true;
+        defaultZone.passIndex = 0;
+        defaultZone.rotation = 0f;
+        defaultZone.updateZoneCenter = new Vector2(0.5f, 0.5f);
+        defaultZone.updateZoneSize = new Vector2(1f, 1f);
+
+        Vector2 fluidCoord = WorldToFluidSpace(other.transform.position.x, other.transform.position.z);
+        var clickZone = new CustomRenderTextureUpdateZone();
+        clickZone.needSwap = true;
+        clickZone.passIndex = 1;
+        clickZone.rotation = 0f;
+        clickZone.updateZoneCenter = fluidCoord;
+        clickZone.updateZoneSize = new Vector2(0.1f, 0.1f);
+
+        waterSimulationRTMaterial.SetFloat("currentForce", 0.1f);
+
+        waterSimulationRT.ClearUpdateZones();
+        waterSimulationRT.SetUpdateZones(new CustomRenderTextureUpdateZone[] { clickZone, defaultZone });
+        waterSimulationRT.Update(2);
     }
 
     void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //Vector2 fluidCoord = WorldToFluidSpace(other.transform.position.x, other.transform.position.z);
-            //fluid.Turbate(fluidCoord.x, fluidCoord.y, playerMovingForce, 2);
+            var defaultZone = new CustomRenderTextureUpdateZone();
+            defaultZone.needSwap = true;
+            defaultZone.passIndex = 0;
+            defaultZone.rotation = 0f;
+            defaultZone.updateZoneCenter = new Vector2(0.5f, 0.5f);
+            defaultZone.updateZoneSize = new Vector2(1f, 1f);
+
+            Vector2 fluidCoord = WorldToFluidSpace(other.transform.position.x, other.transform.position.z);
+            var clickZone = new CustomRenderTextureUpdateZone();
+            clickZone.needSwap = true;
+            clickZone.passIndex = 1;
+            clickZone.rotation = 0f;
+            clickZone.updateZoneCenter = fluidCoord;
+            clickZone.updateZoneSize = new Vector2(0.1f, 0.1f);
+
+            waterSimulationRTMaterial.SetFloat("currentForce", 0.025f);
+
+            waterSimulationRT.ClearUpdateZones();
+            waterSimulationRT.SetUpdateZones(new CustomRenderTextureUpdateZone[] { clickZone, defaultZone });
+            waterSimulationRT.Update();
         }
     }
 
-    //private Vector2 WorldToFluidSpace(float worldX, float worldY)
-    //{
-    //    return new Vector2(
-    //        Mathf.InverseLerp(minPlaneX, maxPlaneX, Mathf.Round((worldX - transform.position.x) / distance) * distance),
-    //        Mathf.InverseLerp(minPlaneY, maxPlaneY, Mathf.Round((worldY - transform.position.z) / distance) * distance));
-    //}
+    private Vector2 WorldToFluidSpace(float worldX, float worldY)
+    {
+        return new Vector2(
+            Mathf.InverseLerp(minPlaneX, maxPlaneX, worldX),
+            1-Mathf.InverseLerp(minPlaneY, maxPlaneY, worldY));
+    }
 }
